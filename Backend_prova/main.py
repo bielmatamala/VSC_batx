a585ecb0fa63b474dfaf8fb206244fc299a53c77
Biel Matamala <biel.matamala.09@ipsi.cat>
2026-01-23 11:53:03 +0100

    Server_propi

--- Backend_prova/main.py
+++ Backend_prova/main.py
@@ -3,6 +3,7 @@ import http
 from fastapi import FastAPI
 import os
 import http
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -10,10 +11,13 @@ app = FastAPI()
 async def HW():
     return "Hello, World!"
 
-@app.get("/documentation")
+@app.get("/doc_1")
 async def documentation():
-    return "http://127.0.0.1:8000/docs"
-    
+    return RedirectResponse(url="http://127.0.0.1:8000/docs")
+
+@app.get("/doc_2")
+async def documentation_2():
+    return RedirectResponse(url="http://127.0.0.1:8000/redoc")
 
 # Documentación con Swagger: http://127.0.0.1:8000/docs
 # Documentación con Redocly: http://127.0.0.1:8000/redoc
