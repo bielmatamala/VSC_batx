275d110cad822a520f93cac0a4507a9a9f5f674f
Bigserg <sergi.cabrera.09@ipsi.cat>
2026-03-09 14:18:36 +0100

    He acabat la meva part

--- SmartGallery/Utils/config.py
+++
@@ -1,4 +0,0 @@
-# Define folder paths for image processing
-INPUT_FOLDER = r"C:\Users\AppJT\Documents\VSC general\SmartGallery\data\input"
-OUTPUT_FOLDER = r"C:\Users\AppJT\Documents\VSC general\SmartGallery\data\output"
-TEMP_FOLDER = r"C:\Users\AppJT\Documents\VSC general\SmartGallery\data\temp"
