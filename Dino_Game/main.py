6957ced664df3e8a3c0f8c7834f5d4a34ad6b2fd
Biel Matamala <Biel.matamala.09@ipsi.cat>
2026-05-04 14:18:38 +0200

    djjh

--- Dino_Game/main.py
+++ Dino_Game/main.py
@@ -16,20 +16,3 @@ while True:
             main()
 
 
-"""
-from Main_Screen_final.main import main_screen_final
-
-if __name__ == "__main__":
-    while True:
-        # 1. Mostra la pantalla inicial
-        resultat = main_screen()
-        if resultat == False:
-            break
-
-        # 2. Executa la partida
-        main()
-
-        # 3. Mostra la pantalla final amb puntuació
-        resultat = main_screen_final()
-        if resultat == False:
-            break"""
